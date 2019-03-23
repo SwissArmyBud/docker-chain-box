ec4190e542f7696ea51cd3903b746cd2b3d043e6
Andrew Porter <swissarmybud@gmail.com>
2019-03-23 12:25:41 -0700

    Var rename for sh

--- Scripts/MigrateChain.sh
+++ Scripts/MigrateChain.sh
@@ -1,5 +1,5 @@
 
 echo "[INFO] -> Migrating contracts onto new chain..."
 # Use Web3 to migrate
-node ${SHScriptRoot}/Scripts/ChainJS/migrate.js $CONTRACTS
+node ${shScriptRoot}/Scripts/ChainJS/migrate.js $CONTRACTS
 echo
