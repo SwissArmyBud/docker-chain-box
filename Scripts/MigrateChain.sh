5ae928041845af3bc9d7013785c9b87e798ce836
Andrew Porter <swissarmybud@gmail.com>
2019-03-23 20:11:18 -0700

    Fix sh until web3 is working

--- Scripts/MigrateChain.sh
+++ Scripts/MigrateChain.sh
@@ -2,4 +2,12 @@
 echo "[INFO] -> Migrating contracts onto new chain..."
 # Use Web3 to migrate
 node ${shScriptRoot}/Scripts/ChainJS/migrate.js $CONTRACTS
+for (( I=1; I<=3; I++ ))
+do
+  node ${shScriptRoot}/Scripts/ChainJS/migrate.js $CONTRACTS
+  EXIT_CODE=$?
+  if [ $EXIT_CODE -eq 0 ]; then
+    I=4
+  fi
+done
 echo
